b4665f0032c6dec94c0d0cef018af9b0375e050d
Bruno Rodrigues <brunohmrodrigues1@hotmail.com>
2023-02-03 11:03:58 -0300

    Feature: Implementing Wallet tests

--- src/tests/Wallet.test.js
+++ src/tests/Wallet.test.js
@@ -23,8 +23,9 @@ beforeEach(() => {
 //   history.push('/carteira');
 });
 
-describe('Test if Wallet page is working as intended', () => {
-  it('Page is rendering Header as intended', () => {
+// describe('Test if Wallet page is working as intended', () => {
+describe('Page is rendering Header as intended', () => {
+  it('Header is rendering email and total expendure', () => {
     const userEmail = screen.getByTestId(email);
     const textExpendure = screen.getByText('Despesa Total: R$');
     const expendure = screen.getByTestId('total-field');
@@ -38,8 +39,10 @@ describe('Test if Wallet page is working as intended', () => {
     expect(expendureCurrency).toBeInTheDocument();
     expect(expendureCurrency.innerHTML).toContain('BRL');
   });
+});
 
-  it('Page is rendering WalletForm as intended', () => {
+describe('Page is rendering WalletForm as intended', () => {
+  it('WalletForm is rendering inputs', () => {
     const inputValue = screen.getByTestId('value-input');
     const inputDescription = screen.getByTestId('description-input');
     const selectCurrency = screen.getByTestId('currency-input');
@@ -61,6 +64,31 @@ describe('Test if Wallet page is working as intended', () => {
   });
 
   it('Page currency field is calling fetch and updating', () => {
-    // expect(fetch).toBeCalled();
+
   });
 });
+
+describe('Page is rendering Table as intended', () => {
+  it('Table is rendering table headers', () => {
+    const description = screen.getByRole('columnheader', { name: 'Descrição' });
+    const tag = screen.getByRole('columnheader', { name: 'Tag' });
+    const method = screen.getByRole('columnheader', { name: 'Método de pagamento' });
+    const value = screen.getByRole('columnheader', { name: 'Valor' });
+    const currency = screen.getByRole('columnheader', { name: 'Moeda' });
+    const exchangeRate = screen.getByText('Câmbio utilizado');
+    const convertedValue = screen.getByText('Valor convertido');
+    const currencyConversion = screen.getByText('Moeda de conversão');
+    const editAndDelete = screen.getByText('Editar/Excluir');
+
+    expect(description).toBeInTheDocument();
+    expect(tag).toBeInTheDocument();
+    expect(method).toBeInTheDocument();
+    expect(value).toBeInTheDocument();
+    expect(currency).toBeInTheDocument();
+    expect(exchangeRate).toBeInTheDocument();
+    expect(convertedValue).toBeInTheDocument();
+    expect(currencyConversion).toBeInTheDocument();
+    expect(editAndDelete).toBeInTheDocument();
+  });
+});
+// });
